1387213c5895aab22244b4733e483910fa223800
francisihe <francis.ihejirika@gmail.com>
2024-10-13 13:47:42 +0100

    Installed godotenv to manage env file, added the env file, added an init function to load the environment variables in the main.go package

--- main.go
+++ main.go
@@ -4,25 +4,47 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/francisihe/golang-task-manager-api/models"
 	"github.com/francisihe/golang-task-manager-api/routes"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
+
+// Function to load the environment variables
+func init() {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
 
 func main() {
 	// Print Hello World message
 	helloWorld()
 
+	// DATABASE VARIABLES
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
 	// ========================
 	// Initialize the database connection
-	dsn := "host=localhost user=postgres password=yourpassword dbname=golang_task_manager port=5432 sslmode=disable"
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPassword, dbName)
+	// dsn := "host=localhost user=postgres password=yourpassword dbname=golang_task_manager port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database: ", err)
 	}
+
+	// Initialize your application with the database connection
+	log.Println("Database connected successfully")
 
 	// Perform auto-migration to create/update tables based on the models
 	db.AutoMigrate(&models.Task{})
