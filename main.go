51889bda80e809e15b532415afc05fbee1975f76
francisihe <francis.ihejirika@gmail.com>
2024-10-13 12:38:44 +0100

    Installed Go ORM GORM, added db initialization in main.go package, added DB variable and adjusted the tassk model to use the gorm key

--- main.go
+++ main.go
@@ -5,12 +5,31 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/francisihe/golang-task-manager-api/models"
 	"github.com/francisihe/golang-task-manager-api/routes"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
 	// Print Hello World message
 	helloWorld()
+
+	// ========================
+	// Initialize the database connection
+	dsn := "host=localhost user=postgres password=yourpassword dbname=golang_task_manager port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+
+	// Perform auto-migration to create/update tables based on the models
+	db.AutoMigrate(&models.Task{})
+
+	// Set the database instance globally -- Instantiated in the models package
+	models.DB = db
+	// ========================
 
 	// Initialize the router
 	router := routes.SetupRouter()
--- models/task.go
+++ models/task.go
@@ -1,13 +1,34 @@
 package models
 
-import "time"
+import (
+	"time"
 
-// Task struct to define the task model
+	"gorm.io/gorm"
+)
+
+// Global variable to hold the database connection
+var DB *gorm.DB
+
+// Task represents a task model with GORM tags for database mapping
 type Task struct {
-	ID          string    `json:"id"`
+	ID          string    `gorm:"primaryKey"`
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
 	Status      string    `json:"status"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
+
+// package models
+
+// import "time"
+
+// // Task struct to define the task model
+// type Task struct {
+// 	ID          string    `json:"id"`
+// 	Title       string    `json:"title"`
+// 	Description string    `json:"description"`
+// 	Status      string    `json:"status"`
+// 	CreatedAt   time.Time `json:"created_at"`
+// 	UpdatedAt   time.Time `json:"updated_at"`
+// }
